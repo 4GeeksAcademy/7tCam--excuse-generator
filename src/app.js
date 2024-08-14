3c1fba2e3d02c194361fbf5fc7fe5959af83a2a2
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-08-14 14:57:39 +0000

    generadorDeExcusaTermninada

--- src/app.js
+++ src/app.js
@@ -5,38 +5,50 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  // almacenar las excusas en un arreglo
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  const $EXCUSA = document.getElementById("excuse");
   // funcion generar excusas
   function excuseGenerator() {
+    // almacenar las excusas en un arreglo
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
     // indices randoms
     let whoIndex = Math.floor(Math.random() * who.length);
     let actionIndex = Math.floor(Math.random() * action.length);
     let whatIndex = Math.floor(Math.random() * what.length);
     let whenIndex = Math.floor(Math.random() * when.length);
-    // retorna la iterpolacion de indices
+
     return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+    // let pintar = document.getElementById("excuse");
+    // pintar.textContent = excuseGenerator();
+    // retorna la iterpolacion de indices
+    // alamecena pero no lo muestra en doc html
+    // return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
   }
+  $EXCUSA.textContent = excuseGenerator();
+  // excuseGenerator();
+
   // llamar a la funcion
-  excuseGenerator();
+  // excuseGenerator(); no es necesario porque se llamara con un boton
   // selecionar al nodo elemento
-  const elemento = document.querySelector("#excuse");
+  // const elemento = document.querySelector("#excuse");
   // acceder al contenido y darle como valor la funcion que nos retorna las excusas aleatorias
-  elemento.textContent = excuseGenerator();
+  // elemento.textContent = excuseGenerator();
+
   // accediendo al boton
-  let boton = document.getElementById(btn);
+  let boton = document.getElementById("btn");
   // agregando evento
-  boton.addEventListener("click", eventoClick);
-  function eventoClick() {
-    return();
-  }
+  // todo lo que queremos ejectuart en el boton debee ir dentro del mismo
+
+  boton.addEventListener("click", () => {
+    $EXCUSA.textContent = excuseGenerator();
+  });
 };
